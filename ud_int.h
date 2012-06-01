87c0074707222c527457a3c086e7089343a6955c
Christopher Haster <chaster@utexas.edu>
2012-06-01 17:01:57 -0500

    Added ud_int

---
+++ ud_int.h
@@ -0,0 +1,26 @@
+//
+// int8
+// uint8
+// int16
+// uint16
+// int32
+// uint32
+// int64
+// uint64
+//
+
+typedef signed char int8;
+typedef unsigned char uint8;
+typedef signed short int16;
+typedef unsigned short uint16;
+typedef signed int int32;
+typedef unsigned int uint32;
+
+#ifdef _MSC_VER
+typedef signed __int64 int64;
+typedef unsigned __int64 uint64;
+#else
+typedef signed long long int64;
+typedef unsigned long long uint64;
+#endif
+
