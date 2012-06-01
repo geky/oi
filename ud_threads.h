e4ca479b3ece64e3da34a8d491cb20d2ddfac4f4
Christopher Haster <chaster@utexas.edu>
2012-06-01 17:15:19 -0500

    Updated ud_threads

--- ud_threads.h
+++ ud_threads.h
@@ -3,13 +3,13 @@
 //
 // thread_t
 //
-// bool thread_create(thread_t * thread, void (* routine)(void *), void * args)
+// int thread_create(thread_t * thread, void (* routine)(void *), void * args)
 //
-// bool thread_join(thread_t thread)
+// int thread_join(thread_t thread)
 //
-// bool thread_terminate(thread_t thread);
+// int thread_terminate(thread_t thread);
 //
-// bool thread_delete(thread_t thread);
+// int thread_delete(thread_t thread);
 //
 
 #ifdef WINDOWS_UD
@@ -17,13 +17,20 @@
 typedef HANDLE thread_t;
 
 #define thread_create(thread, routine, args) \
-	!(*(thread) = _beginthreadex(0,0,&_ud_thread_handler,_ud_thread_args(routine,args),0,0))		
+	_ud_thread_init(thread,routine,args)
 #define thread_join(thread) \
 	WaitForSingleObject(thread, INFINITE)
 #define thread_terminate(thread) \
 	!TerminateThread(thread, 0)
 #define thread_delete(thread) \
 	!CloseHandle(thread);
+
+int _ud_thread_init(thread_t * t, void (*r)(void*), void * a) {
+	void ** mem = malloc(sizeof r + sizeof a);
+	mem[0] = r;  mem[1] = a;
+	*t = _beginthreadex(0,0,&_ud_thread_handler,mem,0,0);
+	return !*t;
+}
 
 unsigned int __stdcall _ud_thread_handler(void ** args) {
 	if (!args) _endthreadex(0);
@@ -39,12 +46,18 @@ typedef pthread_t thread_t;
 
 
 #define thread_create(thread, routine, args) \
-	pthread_create(thread,0,&_ud_thread_handler,_ud_thread_args(routine,args))
+	_ud_thread_init(thread,routine,args)
 #define thread_join(thread) \
 	pthread_join(thread,0)
 #define thread_terminate(thread) \
 	pthread_cancel(thread)
 #define thread_delete 0
+
+int _ud_thread_init(thread_t * t, void (*r)(void*), void * a) {
+	void ** mem = malloc(sizeof r + sizeof a);
+	mem[0] = r;  mem[1] = a;
+	return pthread_create(thread,0,&_ud_thread_handler,mem);
+}
 
 void * _ud_thread_handler(void ** args) {
 	if (!args) return 0;
@@ -56,9 +69,3 @@ void * _ud_thread_handler(void ** args) {
 
 #endif
 
-void * _ud_thread_args(void (*routine)(void*), void * args) {
-	void ** mem = malloc(sizeof(routine)+sizeof(args));
-	mem[0] = routine;
-	mem[1] = args;
-	return mem;
-}
