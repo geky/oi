5d9af3d1c1f527bf0d655c0f70c5ea6e8b4a577e
Christopher Haster <chaster@utexas.edu>
2012-06-06 02:28:58 -0500

    Added oi_socket. Still in working on it.

--- oi_os.h
+++ oi_os.h
@@ -2,22 +2,22 @@
 #define OI_OS
 
 #if defined(_WIN32) || defined(__WIN32__)
-	#define OI_WIN
-	#ifndef WIN32_LEAN_AND_MEAN
-	#define WIN32_LEAN_AND_MEAN
-	#endif
-        #include <windows.h>
+#       define OI_WIN
+#       ifndef WIN32_LEAN_AND_MEAN
+#       define WIN32_LEAN_AND_MEAN
+#       endif
+#       include <windows.h>
 #elif defined(linux) || defined(__linux)
-	#define OI_LINUX
-	#ifndef _XOPEN_SOURCE
-	#define _XOPEN_SOURCE 500
-	#endif
+#       define OI_LINUX
+#       ifndef _XOPEN_SOURCE
+#       define _XOPEN_SOURCE 500
+#       endif
 #elif defined(__APPLE__) || defined(MACOSX) || defined(macintosh) || defined(Macintosh)
-	#define OI_MAC
+#       define OI_MAC
 #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-	#define OI_FreeBSD
+#       define OI_FreeBSD
 #else
-	#error oi does not recognize OS
+#       error oi does not recognize OS
 #endif
 #endif
 
---
+++ oi_socket.h
@@ -0,0 +1,73 @@
+#ifndef OI_SOCKET
+#define OI_SOCKET
+#include "oi_os.h"
+#include "oi_types.h"
+
+#ifdef OI_WIN
+#include <winsock2.h>
+#else
+
+#endif
+
+#define SOCKET_UDP SOCK_DGRAM
+#define SOCKET_TCP SOCK_STREAM
+#define SOCKET_IP  SOCK_RAW
+
+typedef sockaddr_in address_t;
+
+static inline int address_create(address_t * a, uint32 ip, uint16 port) {
+    memset(a->sin_zero,0,8);
+    a->sin_family = AF_INET;
+    a->sin_addr.s_addr = htonl(ip);
+    a->sin_port = htons(port);
+    return 0;
+}
+
+#ifdef OI_WIN
+
+typedef SOCKET socket_t;
+
+static inline int socket_create(socket_t * s, int proto, int block) {
+    *s = socket(AF_INET, proto, 0);
+    u_long blockval = block ? 0 : 1;
+    return ioctlsocket(s,FIONBIO,&blockval) || (s != -1);
+}
+
+static inline int socket_destroy(socket_t * s) {
+    return closesocket(s);
+}
+
+#else
+
+typedef int socket_t;
+
+static inline int socket_create(socket_t * s, int proto, int block) {
+    *s = socket(AF_INET, proto, 0);
+    if (block) fcntl(*s, F_SETFL, fcntl(*s, F_GETFL)&~O_NONBLOCK);
+    return s != -1;
+}
+
+static inline int socket_destroy(socket_t * s) {
+    return close(s);
+}
+
+#endif
+
+static inline int socket_bind(socket_t) {
+    
+}
+
+static inline int socket_send(socket_t * s, void * buf, size_t len, address_t * a) {
+    return sendto(*s,buf,len,0,a,sizeof address_t) == -1;
+}
+
+static inline int socket_recv(socket_t * s, void * buf, size_t len, address_t * a, size_t * inlen) {
+    ssize_t rsize = recvfrom(*s,buf,len,a,sizeof address_t);
+    if (rsize >= 0) *inlen = rsize;
+    return rsize < 0;
+}
+
+
+//#endif
+
+#endif
