d4e8da949afd0712aa2c4e466d7b8c33980ae944
Christopher Haster <chaster@utexas.edu>
2012-06-16 14:28:36 -0500

    Changed oi_types to use stdint.h if possible

--- oi_types.h
+++ oi_types.h
@@ -1,33 +1,25 @@
-//
-// int8
-// uint8
-// int16
-// uint16
-// int32
-// uint32
-// int64
-// uint64
-//
-// float32
-// float64
-//
-
 #ifndef OI_TYPES
 #define OI_TYPES
 
-typedef signed char int8;
-typedef unsigned char uint8;
-typedef signed short int16;
-typedef unsigned short uint16;
-typedef signed int int32;
-typedef unsigned int uint32;
-
-#ifdef _MSC_VER
-typedef signed __int64 int64;
-typedef unsigned __int64 uint64;
+#ifndef _MSC_VER
+#include "stdint.h"
+typedef int8_t   int8;
+typedef uint8_t  uint8;
+typedef int16_t  int16;
+typedef uint16_t uint16;
+typedef int32_t  int32;
+typedef uint32_t uint32;
+typedef int64_t  int64;
+typedef uint64_t uint64;
 #else
-typedef signed long long int64;
-typedef unsigned long long uint64;
+typedef signed char      int8;
+typedef unsigned char    uint8;
+typedef signed short     int16;
+typedef unsigned short   uint16;
+typedef signed int       int32;
+typedef unsigned int     uint32;
+typedef signed __int64   int64;
+typedef unsigned __int64 uint64;
 #endif
 
 typedef float float32;
