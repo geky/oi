e9ae0bbcd9322546b1953c0b84ef0d31cc2491f0
Christopher Haster <chaster@utexas.edu>
2012-09-23 18:43:38 -0500

    Began implementing a select system for sockets

--- oi_udp.h
+++ oi_udp.h
@@ -75,39 +75,39 @@ oi_call udp_rec(socket_t * s, void * buf, size_t * len, address_t * na) {
 }
 
 // returns ERR_TIMEOUT on timeout
-oi_call udp_timed_rec(socket_t * s, void * buf, size_t * len, address_t * na, unsigned int ms) {
+oi_call udp_select_rec(socket_t * dst, void * buf, size_t * len, address_t * na, unsigned int ms, int num, ...) {
     size_t na_s = sizeof(address_t);
     address_t dump;
     int newlen = *len;
+    va_list vlst;
     fd_set fset;
     struct timeval time;
-    _oi_sock max;
+    _oi_sock max = 0;
 
     *len = 0;
+    va_start(vlst,&num);
     FD_ZERO(&fset);
     time.tv_usec = (ms%1000)*1000;
     time.tv_sec = (ms/1000);
 
-#if defined(OI_SINGLESTACK)    
-    if (s->ipv4 == _OI_SINVAL) {
-        FD_SET(s->ipv6,&fset);       
-        max = s->ipv6;
-    } else if (s->ipv6 == _OI_SINVAL) {
-        FD_SET(s->ipv4,&fset);       
-        max = s->ipv4;
-    } else {
-        FD_SET(s->ipv4,&fset);
-        FD_SET(s->ipv6,&fset);
-        max = s->ipv6 > s->ipv4 ? s->ipv6 : s->ipv4;
-    }
-#else
-    FD_SET(s->ipv6,&fset);
-    max = s->ipv6;
+    while (num--) {
+#if defined(OI_SINGLESTACK) 
+        socket_t * next = va_arg(vlst, socket_t*);
+        
+        if (next->ipv4 != _OI_SINVAL) {
+            FD_SET(s->ipv4, &fset);
+            if (next->ipv4 > max) max = next->ipv4;
+        }
 #endif
+        if (next->ipv6 != _OI_SINVAL) {
+            FD_SET(s->ipv6,&fset);
+            if (next->ipv6 > max) max = next->ipv6;
+        }
+    }
     
     if (0 > select(max+1, &fset, 0, 0, &time))
         return _OI_NET_ERR;
-        
+        //TODO below heres
     if (FD_ISSET(s->ipv6,&fset)) {
         newlen = recvfrom(s->ipv6, buf, newlen,0, na?&na->raw:&dump.raw, &na_s);
 #if defined(OI_SINGLESTACK)
